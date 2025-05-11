9cc20b64fd1cca4c3f4f39252d248a708545bbb6
Harris <harrisaminjutt@gmail.com>
2025-05-11 21:56:47 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 const app = express();
 app.use(express.json());
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000");
 });
