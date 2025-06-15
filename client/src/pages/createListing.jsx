a57f33e2d5e4712a1189477e7b694d8d1dd4b05b
Harris <harrisaminjutt@gmail.com>
2025-06-15 17:29:39 +0500

    upload  listing pictures

--- client/src/pages/createListing.jsx
+++ client/src/pages/createListing.jsx
@@ -1,6 +1,57 @@
 import React from "react";
+import { useState } from "react";
 
 function CreateListing() {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const handleImageSubmit = async (e) => {
+    e.preventDefault();
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      try {
+        setUploading(true);
+        const urls = await Promise.all(files.map((file) => storeImage(file)));
+        console.log(urls);
+        setFormData((prev) => ({
+          ...prev,
+          imageUrls: prev.imageUrls ? [...prev.imageUrls, ...urls] : urls,
+        }));
+        setImageUploadError(false);
+        setUploading(false);
+      } catch (err) {
+        setImageUploadError("Failed! Max 2 MB per image");
+        setUploading(false);
+      }
+    } else setImageUploadError("You can only upload 6 images per listing");
+  };
+
+  const handleRemoveImage = (idx) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== idx),
+    });
+  };
+
+  const storeImage = async (file) => {
+    const formData = new FormData();
+    formData.append("image", file);
+    try {
+      const response = await fetch("/api/user/upload", {
+        method: "POST",
+        body: formData,
+      });
+      const data = await response.json();
+      if (!response.ok) throw new Error(data.message || "Image upload failed");
+      return data.url;
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      throw error;
+    }
+  };
+
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -111,16 +162,45 @@ function CreateListing() {
           </p>
           <div className="flex gap-4">
             <input
+              onChange={(e) => setFiles(Array.from(e.target.files))}
               className="p-3 border border-gray-300 rounded w-full"
               type="file"
               id="images"
               accept="images/*"
               multiple
             />
-            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
-              Upload
+            <button
+              onClick={handleImageSubmit}
+              type="button"
+              disabled={uploading}
+              className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+            >
+              {uploading ? "uploading..." : "upload"}
             </button>
           </div>
+          <p className="text-red-700 text-center">
+            {imageUploadError && imageUploadError}
+          </p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, idx) => (
+              <div
+                key={url}
+                className="flex justify-between p-3 items-center border"
+              >
+                <img
+                  src={url}
+                  alt="listing image"
+                  className="w-20 h-20 object-contain rounded-lg"
+                />
+                <button
+                  type="button"
+                  onClick={() => handleRemoveImage(idx)}
+                  className="p-3 text-red-700 rounded-lg uppercase hover:opacity-95"
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
           <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
             Create Listing
           </button>
