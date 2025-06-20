30ee3cd35cc9e7af9b7bfb570f90c015e61dd002
Harris <harrisaminjutt@gmail.com>
2025-06-20 16:58:40 +0500

    create search api route

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -5,6 +5,7 @@ const {
   deleteListing,
   updateListings,
   getListing,
+  getListings,
 } = require("../controllers/listingController");
 
 const router = express.Router();
@@ -13,5 +14,6 @@ router.post("/create", verify, createListing);
 router.delete("/delete/:id", verify, deleteListing);
 router.post("/update/:id", verify, updateListings);
 router.get("/getListing/:listingId", getListing);
+router.get("/get", getListings);
 
 module.exports = router;
--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
 export default function Contact({ listing }) {
   const [landlord, setLandlord] = useState(null);
